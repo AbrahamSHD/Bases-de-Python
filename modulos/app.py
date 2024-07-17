2a3cfec0907591765829c99903d371825864f07f
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-07-17 14:43:36 -0600

    Modulos agregados

---
+++ modulos/app.py
@@ -0,0 +1,9 @@
+# import usuarios.acciones
+# from usuarios import acciones
+from usuarios.acciones.utilidades import guardar
+
+# guardar()
+# pagar_impuestos()
+# usuarios.acciones.guardar()
+guardar()
+print(__name__)
